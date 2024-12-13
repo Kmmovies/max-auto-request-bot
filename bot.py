dc7628d9ba274dfe7d795f06dff91afb80f822b7
KcCinemagalu <124599828+KcCinemagalu@users.noreply.github.com>
2024-12-13 08:04:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ async def start_handler(c, m):
     user_id = m.from_user.id
     if not await Data.find_one({'id': user_id}): await Data.insert_one({'id': user_id})
     lazydeveloper_btn = [[
-        InlineKeyboardButton('Kannada Rockers', url='https://t.me/+CImBn0blbuVhMmM9')
+        InlineKeyboardButton('Kannada Rockers', url='https://t.me/+Kv_WVH4KpXZmNWRl')
     ]]
     return await m.reply_text(text=START_TEXT.format(m.from_user.mention), disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(lazydeveloper_btn))
           
